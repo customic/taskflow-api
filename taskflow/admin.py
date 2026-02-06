e2e36cf597d1ee073cc71f64d46a77204d130b5d
Piotr Kolibabski <piotr.kolibabski@gmail.com>
2026-02-06 20:49:14 +0100

    Register models in Django admin and update settings for environment variables

--- taskflow/admin.py
+++ taskflow/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Label, Project, Task
+
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(Label)
+admin.site.register(Comment)
